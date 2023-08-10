10835c668d6328240216d43bbb68c65bb7c317fa
Tim Rickwood <tim.rickwood@nhs.net>
2023-08-10 19:34:18 +0100

    DR-340 Add boot strapper

---
+++ infrastructure/local/locals.tf
@@ -0,0 +1,7 @@
+# ==============================================================================
+# Context
+
+locals {
+  terraform-git-repo = "uec-dos-service-search"
+  account_id         = data.aws_caller_identity.current.id
+}
