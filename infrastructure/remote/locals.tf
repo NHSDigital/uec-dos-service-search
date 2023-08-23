42f12ba4f262c0a6beab4f5c746290c9a6643069
Tim Rickwood <tim.rickwood@nhs.net>
2023-08-23 15:03:12 +0100

    DR-357 Refactor common tf files

--- infrastructure/remote/locals.tf
+++
@@ -1,6 +0,0 @@
-# ==============================================================================
-# Context
-
-locals {
-  account_id = data.aws_caller_identity.current.id
-}
--- scripts/bootstrapper.sh
+++ scripts/bootstrapper.sh
@@ -84,9 +84,9 @@ if ! $USE_REMOTE_STATE_STORE  ; then
     ROOT_DIR=$PWD
     STACK_DIR=$PWD/$INFRASTRUCTURE_DIR/stacks/$STACK
     cd "$STACK_DIR" || exit
-    cp "$ROOT_DIR"/"$INFRASTRUCTURE_DIR"/remote/versions.tf "$STACK_DIR"
-    cp "$ROOT_DIR"/"$INFRASTRUCTURE_DIR"/remote/locals.tf "$STACK_DIR"
-    cp "$ROOT_DIR"/"$INFRASTRUCTURE_DIR"/remote/provider.tf "$STACK_DIR"
+    cp "$ROOT_DIR"/"$INFRASTRUCTURE_DIR"/common/versions.tf "$STACK_DIR"
+    cp "$ROOT_DIR"/"$INFRASTRUCTURE_DIR"/common/locals.tf "$STACK_DIR"
+    cp "$ROOT_DIR"/"$INFRASTRUCTURE_DIR"/common/provider.tf "$STACK_DIR"
     # run terraform init with migrate flag set
     terraform-init-migrate "$STACK" "$USE_REMOTE_STATE_STORE"
     # now push local state to remote
