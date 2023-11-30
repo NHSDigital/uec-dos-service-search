7c0334ca0c59bd02e4c58f879e292886c238829d
pira Maheswaran <piravinth.maheswaran1@nhs.net>
2023-11-30 16:01:45 +0000

    DR-562 Infra cleardown pipeline

--- scripts/functions/git-functions.sh
+++ scripts/functions/git-functions.sh
@@ -79,7 +79,8 @@ function export_terraform_workspace_name {
           TERRAFORM_WORKSPACE_NAME=$(echo "$DEPLOYMENT_WORKSPACE" | tr "." "-")
         fi
     else
-      BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+      BRANCH_NAME="${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}"
+      BRANCH_NAME=$(echo $BRANCH_NAME | sed 's/refs\/heads\/task/task/g')
       if [ "$BRANCH_NAME" != 'main' ] && [[ $BRANCH_NAME =~ $GIT_BRANCH_PATTERN ]]  ; then
         IFS='/' read -r -a name_array <<< "$BRANCH_NAME"
         IFS='_' read -r -a ref <<< "${name_array[1]}"
@@ -104,5 +105,3 @@ function export_short_commit_hash {
     COMMIT_HASH_SHORT="$(git rev-parse --short HEAD)"
     export COMMIT_HASH_SHORT
 }
-
-
