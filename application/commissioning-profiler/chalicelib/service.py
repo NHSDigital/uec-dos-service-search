e53cd2fe4581a49ccf10c2d99e84df3b1748e2ce
stuartreid <stuart.reid5@nhs.net>
2023-12-07 15:37:26 +0000

    DR-542 Search comm pro

--- application/commissioning-profiler/chalicelib/service.py
+++ application/commissioning-profiler/chalicelib/service.py
@@ -1,2 +1,51 @@
+# chalicelib/service.py
+
+from chalice import Chalice
+
+app = Chalice(app_name="commissioning-profiler")
+
+
 def basic_lambda_handler(event, context):
-    return {"statusCode": 200, "body": "Hello from the Lambda function!"}
+    """
+    Basic Lambda handler function.
+
+    Args:
+    - event: The event data.
+    - context: The context object.
+
+    Returns:
+    A dictionary representing the Lambda function response.
+    """
+    # Example: Extracting data from the event
+    body = event.get("body", {})
+    key = body.get("key", "")
+
+    # Example: Processing the data
+    result_body = f"Received key: {key}"
+
+    # Example: Building the Lambda response
+    response = {
+        "statusCode": 200,
+        "body": result_body,
+        "headers": {"Content-Type": "application/json"},
+    }
+
+    return response
+
+
+@app.route("/")
+def index():
+    return {"hello": "world"}
+
+
+@app.route("/lambda", methods=["POST"])
+def lambda_function():
+    request = app.current_request
+    event = {
+        "body": request.json_body,
+        "headers": request.headers,
+        "httpMethod": request.method,
+        "path": request.context["resourcePath"],
+    }
+    context = {}
+    return basic_lambda_handler(event, context)
---
+++ application/commissioning-profiler/chalicelib/utilities.py
@@ -0,0 +1,2 @@
+# getting the error listed when running ./scripts/unit-tests.sh so put this dummy file in.
+# No source for code: '/uec-dos-service-search/application/commissioning-profiler/chalicelib/utilities.py'.
