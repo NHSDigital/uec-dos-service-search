45bc5945a201e4f07238f2a8ae734bd21d672361
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2024-08-30 11:30:00 +0100

    DR-000 Removing old application code

--- application/commissioning-profiler/chalicelib/handler.py
+++
@@ -1,2 +0,0 @@
-# getting the error listed when running ./scripts/unit-tests.sh so put this dummy file in.
-# No source for code: '/uec-dos-service-search/application/commissioning-profiler/chalicelib/handler.py'.
